513642b65d948d72f3ca2eed6b522234f246219d
GbFerrera <124904382+GbFerrera@users.noreply.github.com>
2024-01-17 16:32:01 -0300

    criando lógicas do controller de users

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,42 +1,70 @@
-const AppError = require("../utils/appError")
+const AppError = require("../utils/appError");
 
-const knex = require("../database/knex")
+const knex = require("../database/knex");
 
 class UsersController {
+  async create(request, response) {
+    const { name, email, password } = request.body;
 
+    if (!name || !email || !password) {
+      throw new AppError("Necessario preencher todos os campos");
+    }
 
-create(request,response){
+    if (password.length < 6) {
+      throw new AppError("A senha deve ter pelo menos 6 caracteres");
+    }
 
-  const {name,email,password} = request.body
+    const emailExist = await knex("users").where("email", email).first();
 
- if(!name){
+    if (emailExist) {
+      throw new AppError("Esse e-mail ja esta em uso!");
+    }
 
- throw new AppError("Nome obrigatório")
- 
+    await knex("users").insert({ name, email, password });
+
+    return response.json("Criado com sucesso");
+  }
+
+  async update(request, response) {
+    const { name, email, newPassword, oldPassword } = request.body;
+    const { id } = request.params;
+
+    const user = await knex("users").where("id", id).first();
+
+    if (email !== user.email) {
+      const verifyEmail = await knex("users").where("email", email).first();
+
+      if (verifyEmail && verifyEmail.id != user.email) {
+        throw new AppError("Este e-mail já esta em uso!");
+      }
+    }
+
+    if (oldPassword != user.password) {
+      throw new AppError("A antiga senha não confere!");
+    }
+
+    if (newPassword.length < 6) {
+      throw new AppError("A nova senha é necessária ter no minimo 6 digitos!");
+    }
+
+    await knex("users").where("id", id).update({
+      name,
+      email,
+      password: newPassword,
+    });
+
+    return response.json("Atualizado com sucesso :)");
+  }
+
+  async show(request, response) {
+    const { id } = request.params;
+
+    const user = await knex("users")
+      .where("id", id)
+      .select("name", "email", "password", "avatar");
+
+    return response.json(user);
+  }
 }
 
- return response.json({name,email,password})
-
-
-}
-
-update(request,response){
-
-  const {name,email,password} = request.body
-  const user = request.params
-
-
-  
-
-}
-
-show(request,response){
-  
-}
-
-
-
-}
-
-
-module.exports = UsersController
+module.exports = UsersController;
